4085e3348b11213c1a69b7bfdca05a770abc0bdc
Bob Donahue <ursomniac@mac.com>
2022-01-23 15:14:05 -0500

    Enhancing management command to force all or use a subset of DSOs

--- skytour/skytour/apps/dso/management/commands/create_dso_finder_charts.py
+++ skytour/skytour/apps/dso/management/commands/create_dso_finder_charts.py
@@ -161,13 +161,44 @@ def create_dso_finder_chart(dso, fov=8, mag0=9, axes=False):
 
 class Command(BaseCommand):
     help = 'Create DSO finder charts'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--dso_list', dest='dso_list', nargs='+', type=int)
+        parser.add_argument('--all', dest='all', action='store_true')
     
-    def handle(self, *args, **kwargs):
-        dsos = DSO.objects.all()
+    def handle(self, *args, **options):
+        """
+        Three ways this can run:
+            - Create all new maps for all DSOs (all = True)
+            - Create/Update maps for a subset of DSOs (dso_list=[ list of PKs ])
+            - Create maps for DSOs that don't already have one (all=False, no dso_list)
+        """
+        dso_list = None
+        all = False
+        just_new = False
+
+        if options['all']:
+            all = options['all']
+            print ("ALL is true")
+        elif options['dso_list']:
+            dso_list = options['dso_list']
+            print ("GOT DSOs: ", dso_list)
+        else:
+            just_new = True
+            print ("Running new DSOs")
+
+        if dso_list:
+            dsos = DSO.objects.filter(pk__in=dso_list)
+        else:
+            dsos = DSO.objects.all()
+
         for dso in dsos:
-            if dso.dso_finder_chart:
+            if just_new and dso.dso_finder_chart:
                 continue
-            print("Creating Finder Chart for {}: {}".format(dso.pk, dso.shown_name))
+            
+            # Otherwise operate!
+            print("Creating/Updating Finder Chart for {}: {}".format(dso.pk, dso.shown_name))
+
             fn = create_dso_finder_chart(dso)
             dso.dso_finder_chart = 'dso_charts/{}'.format(fn)
             #print ("\tFN: ", fn)
