202e24370cc456a6b217bed6ee95b747fd652e37
Bob Donahue <ursomniac@mac.com>
2025-02-16 11:38:54 -0500

    Oops - forgot to git add...

---
+++ skytour/skytour/apps/misc/urls_calendar.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (
+    CalendarYearView, CalendarMonthView
+)
+
+urlpatterns = (
+    path('', CalendarMonthView.as_view(), name='calendar-current'),
+    path('<int:year>/', CalendarYearView.as_view(), name='calendar-year'),
+    path('<int:year>/<str:month>', CalendarMonthView.as_view(), name='calendar-month'),
+)
